19bd6d5768560b9b133a09fed8f2e65746c919b4
wangke <945352629@qq.com>
2020-08-18 14:55:02 +0800

    idea创建git

---
+++ git-test-project/src/com/cn/test/FirstClass.java
@@ -0,0 +1,12 @@
+package com.cn.test;
+
+public class FirstClass{
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+    }
+}
