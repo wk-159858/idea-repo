e4c9f73713dea9f648e1c6e59d4c8344ed565c01
wangke <945352629@qq.com>
2020-08-18 15:09:02 +0800

    修改远程仓库

--- git-test-project/src/com/cn/test/FirstClass.java
+++ git-test-project/src/com/cn/test/FirstClass.java
@@ -7,10 +7,12 @@ public class FirstClass{
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //hellotest
     }
     public void test(){
         int i = 0;
         i+= 10 ;
+        System.out.println(i);
     }
 }
 
